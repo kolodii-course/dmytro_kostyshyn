ea9bd8fbd6c31501bb882080846485277f69a99c
Dmytro Kostyshyn <dima.kostishen@gmail.com>
2023-04-27 00:58:02 +0300

    Added sumple react app with style component lib.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Breadcrumb, Layout, Menu, theme } from "antd";
+
+const { Header, Content, Footer } = Layout;
+
+const App = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  return (
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["2"]}
+          items={new Array(4).fill(null).map((_, index) => {
+            const key = index + 1;
+            return {
+              key,
+              label: `nav ${key}`,
+            };
+          })}
+        />
+      </Header>
+
+      <Content style={{ padding: "0 50px" }}>
+        <div
+          className="site-layout-content"
+          style={{ background: colorBgContainer }}
+        >
+          Conteasfasfasfsnt
+        </div>
+      </Content>
+
+      <Footer style={{ textAlign: "center" }}>
+        Ant Design ©2023 Created by Ant UED
+      </Footer>
+    </Layout>
+  );
+};
+
+export default App;
